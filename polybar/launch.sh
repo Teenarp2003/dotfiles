25aee65829d997c3bc657fa454b0f96042607697
Praneet Guha Roy <praneet.guharoy@gmail.com>
2023-01-10 14:55:12 +0530

    dotfiles: add dots

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances and env programs
+killall -q polybar dunst lxpolkit glava  
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+#Nitrogen
+nitrogen --restore
+# Launch picom
+picom -b
+# Launch
+polybar top &
+echo "Bar launched..."
---
+++ polybar/scripts/player.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+player_status=$(playerctl status 2> /dev/null)
+song_name=$(playerctl metadata title --no-messages)
+song_name_mod=${song_name:0:12}"..."
+song_name_set=$(playerctl metadata title --no-messages |wc -w )
+
+artist_name=$(playerctl metadata artist --no-messages)
+artist_name_len=$(playerctl metadata artist --no-messages | wc -c)
+artist_name_mod=$(playerctl metadata artist --no-messages| head -n1 | awk '{print $1;}')
+
+if [ "$song_name_set" -gt "2" ]; then
+  song_name_disp=${song_name_mod}
+else
+  song_name_disp=${song_name}
+fi
+
+if [ "$artist_name_len" -gt "10" ]; then
+  artist_name_disp=${artist_name_mod}
+else
+  artist_name_disp=${artist_name}
+fi
+
+if [ "$player_status" = "Playing" ]; then
+  echo "${artist_name_disp}  ${song_name_disp}"
+elif [ "$player_status" = "Paused" ]; then
+  echo "${artist_name_disp}  ${song_name_disp}"
+else
+    echo ""
+fi
