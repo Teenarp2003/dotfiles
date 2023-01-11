3d63591bfb3aa9f5847e918bbadb0b16f8c4f41b
Praneet Guha Roy <praneet.guharoy@gmail.com>
2023-01-12 02:59:19 +0530

    dotfiles: fix polybar eth module ; add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -S --noconfirm bspwm sxhkd polybar picom alacritty rofi glava lxsession dunst nitrogen adobe-source-code-pro-fonts playerctl
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+rm -rf yay-git
+yay -S --noconfirm nerd-fonts-cascadia-code nerd-fonts-meslo nerd-fonts-mononoki
+cp -r ~/dotfiles ~/.config
