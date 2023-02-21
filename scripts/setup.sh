d85251d156e978c4a2463ece6d6a463c70b6134e
Praneet Guha Roy <praneet.guharoy@gmail.com>
2023-02-21 19:37:45 +0530

    dotfiles: update repo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-makepkg -si
-rm -rf yay-git
 mkdir ~/bin
 sudo pacman -S python-pywal
-
 cp ~/.config/scripts-custom/colors-alacritty.yml /usr/lib/python3.10/site-packages/pywal/templates/
 cp ~/.config/scripts-custom/colors.ini /usr/lib/python3.10/site-packages/pywal/templates/
 cp ~/.config/scripts-custom/colors.rasi /usr/lib/python3.10/site-packages/pywal/templates/
-
 cp ~/.config/scripts-custom/randwal ~/bin/
 cp ~/.config/scripts-custom/glava-colors ~/bin/
 cp ~/.config/scripts-custom/brightness ~/bin/
