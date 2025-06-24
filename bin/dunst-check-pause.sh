4d5a8d51ad0e095921cbf231b2e79255a4d0fb17
Praneet Guha Roy <praneet.guharoy@gmail.com>
2025-06-25 03:20:42 +0530

    add focus mode script

--- bin/dunst-check-pause.sh
+++ bin/dunst-check-pause.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+dunst_status=$(dunstctl is-paused)
 
-dunst_status= $(dunstctl is-paused)
-dunst_off= $(dunstctl set-paused toggle)
+notif_off=(notify-send.sh -t 1000 --print-id --replace=10 "Focus Mode OFF")
+notif_on=(notify-send.sh -t 1000 --print-id --replace=10 "Focus Mode ON")
+dunst_toggle=(dunstctl set-paused toggle)
+
 if [[ "$dunst_status" == "false" ]]; then
-  notify-send.sh -t 500 --print-id --replace=10 "Focus Mode OFF" && $dunst_off
-elif [[ "$dunst_status" == "true" ]]; then
-  notify-send.sh -t 500 --print-id --replace=10 "Focus Mode ON" && $dunst_off
+  "${notif_on[@]}"
+  sleep 0.5s
+  "${dunst_toggle[@]}"
+else
+  "${notif_off[@]}"
+  sleep 0.2s
+  "${dunst_toggle[@]}"
 fi
- 
+
